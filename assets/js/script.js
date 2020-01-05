844e62e1f8ad7453c5a07513f3d86b674c0b92e9
alexanderzhong <alexhyzhong@gmail.com>
2020-01-05 15:03:35 -0500

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,8 +6,22 @@ function LinkedIn() {
 	window.open("https://www.linkedin.com/in/alexander-zhong-331b79173/");
 }
 
+var resVis = false;
 function Resume() {
-	window.open("assets/documents/SophomoreResumeV2.pdf", "_self");
+	// window.open("assets/documents/SophomoreResumeV2.pdf", "_self");
+	$('#resume_body').fadeIn(300);
+	$('#resume_body').toggleClass('fadeDown');
+	$('#bio-button').fadeOut(100);
+	setTimeout(function () {
+	resVis = true;
+}, 600);
+}
+
+function closeResume() {
+	$('#resume_body').fadeOut(300);
+	setTimeout(function () {$('#resume_body').toggleClass('fadeDown');}, 300);
+	$('#bio-button').fadeIn(100);
+	resVis = false;
 }
 
 function Paper() {
@@ -17,21 +31,17 @@ function Paper() {
 
 /* Information */
 
-function revealBio() {
+function descend() {
+	$('.overlay').delay(1500).fadeOut(500);
 	$('.name').fadeOut(500);
-	$('#bio').delay(3500).fadeIn(500);
-
-	// var stars = document.getElementById('stars');
-	// var clouds = document.getElementById('clouds');
-	// if (document.getElementById('background2').classList.contains('goUp')) {
-	// 	$('.background').removeClass('goUp');
-	// 	$('#background2').removeClass('goUp');
-	// 	$('#background2-extra').removeClass('goUp');
-	// }
 	$('.twinkling').fadeOut(200);
 	$('.background').addClass('goDown');
-	$('#background2').addClass('goDown');
+	$('.background2').addClass('goDown');
 	$('#bio-button').addClass('disappearUp');
+
+	// $('.links').show();
+	$('.links').addClass('goDown');
+
 	setTimeout(function () {
 		$('#bio-button').hide();
 	}, 400);
@@ -45,8 +55,6 @@ function revealBio() {
 	setTimeout(function () {
 		$('#building1').show();
 		$('#building1').addClass('goDown');
-		$('#paper').show();
-		$('#paper').addClass('goDown');
 	}, 1500);
 	setTimeout(function () {
 		$('#building2').show();
@@ -60,18 +68,14 @@ function revealBio() {
 }
 
 function goBack() {
-	if (document.getElementById('background2').classList.contains('goDown')) {
-		$('.background').removeClass('goDown');
-		$('#background2').removeClass('goDown');
-		$('#background2-extra').removeClass('goDown');
-	}
-	$('#bio').fadeOut(500);
-	$('.name').delay(2000).fadeIn(500);
+	$('.overlay').fadeIn(500);
+	$('#bio').fadeOut(300);
+	$('.name').delay(1500).fadeIn(500);
 
 
 	$('.twinkling').delay(2000).fadeIn(100);
 	$('.background').removeClass('goDown');
-	$('#background2').removeClass('goDown');
+	$('.background2').removeClass('goDown');
 	$('#back-button').addClass('disappearDown');
 	setTimeout(function () {
 		$('#back-button').hide();
@@ -88,13 +92,45 @@ function goBack() {
    	// $('#building2').fadeOut(500);
     $('#bridge').removeClass('goDown');
     $('#building1').removeClass('goDown');
-    $('#paper').removeClass('goDown');
     $('#building2').removeClass('goDown');
    }, 400);
+    $('.links').removeClass('goDown');
 	
 
 	// $('#back-button').addClass('disappearDown');
 }
+
+var bio_flag = false;
+function bioToggle() {
+	if(bio_flag) {
+		$('#bio').fadeOut(200);
+		bio_flag = false;
+		setTimeout(function () {
+		$('#bio').toggleClass('fadeDown');
+		}, 200);
+	} else if(!bio_flag && !contact_flag) {
+		$('#bio').fadeIn(200);
+		$('#bio').toggleClass('fadeDown');
+		bio_flag = true;
+	}
+}
+
+var contact_flag = false;
+function contactToggle() {
+	if(contact_flag) {
+		$('#contact').fadeOut(500);
+		contact_flag = false;
+	} else if(!contact_flag && !bio_flag){
+		$('#contact').fadeIn(500);
+		contact_flag = true;
+	}
+}
+
+function openMail() {
+	window.open("mailto:azhong@andrew.cmu.edu?Subject=Contact%20From%20Website", "_top");
+}
+
+	// $('#bio').fadeOut(500);
 
 /* Icon Animations */
 
@@ -138,3 +174,84 @@ $("#rButton").hover(revealResume, hideResume);
 // Bio stuff
 
 
+$(".links").hover(function() {
+	var color = $(this).css('color');
+  $(this).css('text-shadow', '0 0 10px ' + color);
+}, function() {
+	var color = $(this).css('color');
+  $(this).css('text-shadow', '0 0 5px ' + color);
+});
+
+var modal = document.getElementById('resume_body');
+window.onclick = function(event) {
+  if (!(event.target == modal) && !(event.target == document.getElementById('resume-header'))) {
+    // closeResume();
+    // $('#resume_body').is(":visible")
+    if (resVis) {
+    	closeResume();
+   	}
+    // modal.style.display = "none";
+  }
+}
+
+
+//Contact Form
+// var fields = {};
+
+// document.addEventListener("DOMContentLoaded", function() {
+//  fields.firstName = document.getElementById('firstName');
+//  fields.lastName = document.getElementById('lastName');
+//  fields.org = document.getElementById('org');
+//  fields.email = document.getElementById('email');
+//  // fields.country = document.getElementById('country');
+//  // fields.password = document.getElementById('password');
+//  // fields.passwordCheck = document.getElementById('passwordCheck');
+//  // fields.newsletter = document.getElementById('newsletter');
+//  // fields.question = document.getElementById('question');
+// })
+
+// function isEmail(email) {
+// 	let regex = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+// 	return regex.test(String(email).toLowerCase());
+// }
+
+// function isNotEmpty(value) {
+//  if (value == null || typeof value == 'undefined' ) return false;
+//  return (value.length > 0);
+// }
+
+// function fieldValidation(field, validationFunction) {
+// 	if (field == null) return false;
+
+// 	let isFieldValid = validationFunction(field.value)
+// 	// if (!isFieldValid) {
+// 	// field.className = 'placeholderRed';
+// 	// } else {
+// 	// field.className = '';
+// 	// }
+
+// 	return isFieldValid;
+// }
+
+// function isValid() {
+// 	var valid = true;
+
+// 	valid &= fieldValidation(fields.firstName, isNotEmpty);
+// 	valid &= fieldValidation(fields.lastName, isNotEmpty);
+// 	valid &= fieldValidation(fields.org, isNotEmpty);
+// 	// valid &= fieldValidation(fields.gender, isNotEmpty);
+// 	// valid &= fieldValidation(fields.address, isNotEmpty);
+// 	// valid &= fieldValidation(fields.country, isNotEmpty);
+// 	valid &= fieldValidation(fields.email, isEmail);
+// 	// valid &= fieldValidation(fields.houseNumber, isNumber);
+// 	// valid &= fieldValidation(fields.password, isPasswordValid);
+// 	// valid &= fieldValidation(fields.passwordCheck, isPasswordValid);
+// 	// valid &= fieldValidation(fields.question, isNotEmpty);
+// 	// valid &= arePasswordsEqual();
+
+//  return valid;
+// }
+
+// function sendContact() {
+
+// }
